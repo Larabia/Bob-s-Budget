be45423a7fd29b96b9b4cb049dbc4d1ea708b8c2
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-07-30 22:24:45 -0300

    weekly expenses

--- index.php
+++ index.php
@@ -11,6 +11,12 @@ $monthlyExpenses = [
     "healthInsurance" => 200
 ];
 
+$weeklyExpenses = [
+    "gas" => 25,
+    "food" => 90,
+    "entertainment" => 47
+];
+
 $grossSalary = 48150;
 $socialSecurity = $grossSalary * .062;
 
@@ -20,21 +26,34 @@ $netIncome = ($incomeSegments[0][0] * $incomeSegments[0][1]) + ($incomeSegments[
 
 $annualIncome = $netIncome - $socialSecurity;
 
-echo "ANUAL income (after taxes) : ".$annualIncome;
+echo "ANUAL income (after taxes) : \n".$annualIncome;
 
 $annualIncome -=$annualExpenses["vacations"];
 $annualIncome -=$annualExpenses["carRepairs"];
 
-echo "\nANUAL income (after taxes and annual expenses) : ".$annualIncome;
+echo "\nANUAL income (after taxes and annual expenses) : \n".$annualIncome;
 
 $monthlyIncome = $annualIncome/12;
 
-echo "\nMONTHLY income (after taxes and annual expenses) : ".$monthlyIncome;
+echo "\nMONTHLY income (after taxes and annual expenses) : \n".$monthlyIncome;
 
 $monthlyIncome -= $monthlyExpenses["rent"];
 $monthlyIncome -= $monthlyExpenses["utilities"];
 $monthlyIncome -= $monthlyExpenses["healthInsurance"];
 
-echo "\nMONTHLY income (after taxes, annual expenses and monthly expenses) : ".$monthlyIncome;
+echo "\nMONTHLY income (after taxes and annual & monthly expenses) : \n".$monthlyIncome;
+
+$weeklyIncome = $monthlyIncome/4.33;
+
+echo "\nWEEKLY income (after taxes and annual & monthly expenses)) : \n".$weeklyIncome;
+
+$weeklyIncome -= $weeklyExpenses["gas"];
+$weeklyIncome -= $weeklyExpenses["food"];
+$weeklyIncome -= $weeklyExpenses["entertainment"];
+
+echo "\nWEEKLY income (after taxes and annual & monthly & weekly expenses) : \n".$weeklyIncome;
+
+
+
 
 
